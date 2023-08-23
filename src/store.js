834999542155c6021d2989c49f5091d4e52a5903
chammar55 <chammar5255@gmail.com>
2023-08-23 23:52:30 +0500

    5

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux"; // it is old method to use redux. NEW method is redux-tool-kit
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,7 +14,12 @@ function reducer({ state = initialState, action }) {
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case "account.payLoan":
       return {
         ...state,
@@ -24,3 +31,47 @@ function reducer({ state = initialState, action }) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+//********************1st method ***************************/
+
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
+// console.log(store.getState());
+
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
+
+//********************2st method ***************************/
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.deposit(deposit(500));
+store.deposit(withdraw(200));
+console.log(store.getState());
+
+store.deposit(requestLoan(1000, "Buy a cheap car"));
+console.log(store.getState());
+store.dispose(payLoan());
+console.log(store.getState());
