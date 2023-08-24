931503dd8a8565b1c4cb6ffca6448270b32179f4
chammar55 <chammar5255@gmail.com>
2023-08-24 21:02:27 +0500

    12

--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
-import { combineReducers, createStore } from "redux"; // it is old method to use redux. NEW method is redux-tool-kit
+import { applyMiddleware, combineReducers, createStore } from "redux"; // it is old method to use redux. NEW method is redux-tool-kit
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
